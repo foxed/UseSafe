40156f0bfb129d9054a3181e6ffcb7357b4d081c
Alex Fox <alexdysonfox@gmail.com>
2014-05-01 13:43:44 -0400

    Styling resource index page and seeding DB with resources
    Refactoring Resource sort login into model

---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,48 @@
+class ResourcesController < ApplicationController
+  def show
+    @resource = Resource.find(params[:id])
+  end
+
+  def index
+    @resources = Resource.sort_by_city
+  end
+
+  def new
+    @resource = Resource.new
+  end
+
+  def create
+    @resource = Resource.create(resources_params)
+    redirect_to @resource
+  end
+
+  def edit
+    @resource = Resource.find(params[:id])
+  end
+
+  def update
+    @resource = Resource.find(params[:id])
+    if @resource.update(resources_params)
+      redirect_to @resource
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    resource = Resource.find(params[:id])
+    resource.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def resources_params
+    params.require(:resource).permit(
+      :name,
+      :role,
+      :city,
+      :address
+    )
+  end
+end
