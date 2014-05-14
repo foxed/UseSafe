78800d182eeee4c8ba6bc7e798740729c8b8a3a9
Alex Fox <alexdysonfox@gmail.com>
2014-05-14 17:08:06 -0400

    Extracting Readings index into partials
    Reducing number of queries on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,13 @@ class PostsController < ApplicationController
   before_filter :ensure_user_is_admin, only: [:create, :new]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user)
   end
 
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
-    @comments = @post.comments.recent
+    @comments = @post.comments.recent.includes(:user, :post)
   end
 
   def new
