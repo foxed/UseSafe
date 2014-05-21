b0fed3c84916b040a11be2243f4f09e7ea93a5ae
Alex Fox <alexdysonfox@gmail.com>
2014-05-21 10:45:05 -0400

    Precompiling assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 UseSafe::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -27,7 +26,7 @@ UseSafe::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
